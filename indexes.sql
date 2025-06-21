f4c9cc3c407004eed2f29896f6ce5635244b3d0b
TaresuSenu <128540875+TaresuSenu@users.noreply.github.com>
2025-06-21 15:41:37 -0300

    Update indexes.sql

--- indexes.sql
+++ indexes.sql
@@ -11,17 +11,17 @@ CREATE INDEX IF NOT EXISTS idx_mensagem_id_usuario
 ON mensagem ( id_usuario ) ;
 
 -- 4) Índice para Caixa de Entrada de Mensagens
-CREATE INDEX idx_destinatario_id_destinatario ON destinatario (
+CREATE INDEX IF NOT EXISTS idx_destinatario_id_destinatario ON destinatario (
 id_destinatario ) ;
 
 -- 5) Índice para Listagem de Alunos por Turma
-CREATE INDEX idx_matricula_id_oferta ON matricula ( id_oferta ) ;
+CREATE INDEX IF NOT EXISTS idx_matricula_id_oferta ON matricula ( id_oferta ) ;
 
 -- 6) Índice para Disciplinas de um Professor
-CREATE INDEX idx_responsavel_disciplina_id_professor
+CREATE INDEX IF NOT EXISTS idx_responsavel_disciplina_id_professor
 ON responsavel_disciplina ( id_professor ) ;
 
 -- 7) Índice para Histórico de Avisos por Funcionário
-CREATE INDEX idx_aviso_id_funcionario
+CREATE INDEX IF NOT EXISTS idx_aviso_id_funcionario
 ON aviso ( id_funcionario_administrativo ) ;
 
